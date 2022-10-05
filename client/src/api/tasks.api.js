06e1c53a41bb177283e6578b5923d326dca76f08
Bryan Sepia <bryanromero@gmail.com>
2022-10-05 13:13:10 -0500

    changing the endpoints

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,37 +1,49 @@
 import axios from "axios";
 
 const createTaskRequest = async (task) => {
-  const response = await axios.post("http://localhost:4000/api/tasks", task);
+  const response = await axios.post(
+    "https://react-vite-tailwind-project-production.up.railway.app/api/tasks",
+    task
+  );
   return response.data;
 };
 
 const getTasksRequest = async () => {
-  const response = await axios.get("http://localhost:4000/api/tasks");
+  const response = await axios.get(
+    "https://react-vite-tailwind-project-production.up.railway.app/api/tasks"
+  );
   return response.data;
 };
 
 const getTaskByIdRequest = async (id) => {
-  const response = await axios.get(`http://localhost:4000/api/tasks/${id}`);
+  const response = await axios.get(
+    `https://react-vite-tailwind-project-production.up.railway.app/api/tasks/${id}`
+  );
   return response.data;
 };
 
 const updateTaskRequest = async (id, task) => {
   const response = await axios.put(
-    `http://localhost:4000/api/tasks/${id}`,
+    `https://react-vite-tailwind-project-production.up.railway.app/api/tasks/${id}`,
     task
   );
   return response.data;
 };
 
 const deleteTaskRequest = async (id) => {
-  const response = await axios.delete(`http://localhost:4000/api/tasks/${id}`);
+  const response = await axios.delete(
+    `https://react-vite-tailwind-project-production.up.railway.app/api/tasks/${id}`
+  );
   return response.data;
 };
 
 const toggleTaskDoneRequest = async (id, done) => {
-  const response = await axios.put(`http://localhost:4000/api/tasks/${id}`, {
-    done,
-  });
+  const response = await axios.put(
+    `https://react-vite-tailwind-project-production.up.railway.app/api/tasks/${id}`,
+    {
+      done,
+    }
+  );
   return response.data;
 };
 
