1332ef5d4603ac5a080162971c6bbc6b9cd46c50
Bryan Sepia <bryanromero@gmail.com>
2022-10-05 10:22:09 -0500

    node_module deleted

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 export default {
-    PORT: process.env.PORT,
-    DB_HOST: process.env.DB_HOST,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_DATABASE: process.env.DB_DATABASE,
+  PORT: process.env.PORT || 4000,
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_USERNAME: process.env.DB_USERNAME || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "147258nf3",
+  DB_DATABASE: process.env.DB_DATABASE || "tasks_db",
 };
